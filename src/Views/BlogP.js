90fe149a2ae2e53110b8390445de85e2cffc35ef
MusSamat <m.abdisamato@gmail.com>
2021-03-17 16:10:50 +0600

    Revert "lala"
    This reverts commit 74c766a074f5a4366ed1b6545ed8b8f580d7d3d6.

--- src/Views/BlogP.js
+++ src/Views/BlogP.js
@@ -1,36 +1,40 @@
 import React, {useEffect, useState} from "react";
-import {Link} from "react-router-dom";
-import GetData from "../../services/GetData";
+import Carousel from "../Components/Main/Carousel";
+import Footer2 from "../Components/Footer/Footer2";
+import GetData from "../services/GetData";
+import CKEditor from 'ckeditor4-react';
 
 const BlogP = () => {
+
     const getBlog = new GetData()
+
     const [blog, setBlog] = useState([])
 
     useEffect(() => {
-        getBlog.getData('/api/v1/blog').then(res => {
+        getBlog.getData('/api/v1/blog/4').then(res => {
             setBlog(res)
         })
     }, [])
-    return(
-        <div>
-            <h2 className="h2">Блог</h2>
-            <div className="row">
-                {blog.map((item, i) => (
-                    <div className="col-sm-4 text-center" key={i}>
-                        <img src={'http://127.0.0.1:8000'+ item.image} className="img-fluid"  alt="fjalksd"/>
-                        <h2 className="m-n"><a href="#">{item.title}</a></h2>
-                        <div>...</div>
-                        <p> Baic.kg &nbsp;|&nbsp; {item.created_at}</p>
+
+    return (
+        <>
+            <div id="main" >
+                <div className="inner">
+                    <Carousel/>
+                    <div>
+                        <h2 className="h2">Блог</h2>
+                        <div className="row">
+                            {blog.title}
+                            <div dangerouslySetInnerHTML={{__html: blog.description}} />
+                        </div>
                     </div>
-                ))}
+                </div>
             </div>
-            <div style={{display: "flex", justifyContent: "center", marginTop: 20}}>
-                <Link to="/blog"><button>Подробнее <i className='fa fa-long-arrow-right'></i></button></Link>
+            <div  id='footer' className='inner'>
+                <Footer2/>
             </div>
-        </div>
+        </>
     )
 }
 
 export default BlogP
-
-
